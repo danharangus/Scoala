63d7d032a67f98fe1284d23b8fa2a85fa27c2c3c
CatalinTermure <32488274+CatalinTermure@users.noreply.github.com>
2017-10-06 13:41:19 +0300

    Update oli2004loc3.cpp

--- oli2004loc3.cpp
+++ oli2004loc3.cpp
@@ -8,7 +8,7 @@ fstream inf("bomboane.in");
 ofstream outf("bomboane.out");
 
 vector<int> v;
-int a, b, t = 0; // a- bobo curenta ; b- cea anterioara; t - lungimea sirului; m - lung sir max
+int a, b, t = 0, m = 1; // a- bobo curenta ; b- cea anterioara; t - lungimea sirului; m - lung sir max
 bool boo = false;
 
 int main() {
@@ -19,14 +19,14 @@ int main() {
             boo = true;
         }
         if(a == b) {
-            if(boo) {
-                v.push_back(t);
+            if(boo && t > m) {
+                m = t;
             }
             t = 0;
             boo = false;
         }
         t++;
     }
-    outf << *max_element(v.begin(), v.begin() + v.size());
+    outf << m;
     return 0;
 }
