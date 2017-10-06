ce313418aeea62518dd86b89af056b753c0beaf6
CatalinTermure <32488274+CatalinTermure@users.noreply.github.com>
2017-10-06 13:38:37 +0300

    Update oli2004loc3.cpp

--- oli2004loc3.cpp
+++ oli2004loc3.cpp
@@ -1,24 +1,32 @@
 #include <fstream>
+#include <vector>
+#include <algorithm>
 
 using namespace std;
 
 fstream inf("bomboane.in");
 ofstream outf("bomboane.out");
 
-int a, b, t, m = 1;
+vector<int> v;
+int a, b, t = 0; // a- bobo curenta ; b- cea anterioara; t - lungimea sirului; m - lung sir max
+bool boo = false;
 
 int main() {
     for(int i = 0; i < 30; i++) {
         b = a;
         inf >> a;
-        if(t > m) {
-            m = t;
+        if(a == 3) {
+            boo = true;
+        }
+        if(a == b) {
+            if(boo) {
+                v.push_back(t);
+            }
+            t = 0;
+            boo = false;
         }
         t++;
-        if(a == b) {
-            t = 0;
-        }
     }
-    outf << m;
+    outf << *max_element(v.begin(), v.begin() + v.size());
     return 0;
 }
